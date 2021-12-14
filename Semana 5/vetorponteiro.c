e93c4d0a391860a38cb2708fcc754d0dc7b42005
Pedro Henrique Barauna <barauna38@gmail.com>
2021-12-14 16:14:19 -0300

    exercicio da semana 5

---
+++ Semana 5/vetorponteiro.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+int main()
+{
+    int *vetor;
+    int i, x, y;
+    x=5000;
+    y=0;
+    for (i=0;i<x;i++){
+        vetor=malloc(x*sizeof(int));
+        vetor[i]=y;
+        y++;
+        printf("%d \n", vetor[i]);
+    }
+}
