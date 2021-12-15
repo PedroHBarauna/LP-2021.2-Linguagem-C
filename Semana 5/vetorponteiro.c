db04d8956458ca9bd4783542e21af981d2d3ae1f
Pedro Henrique Barauna <barauna38@gmail.com>
2021-12-15 19:47:59 -0300

    correção total do exercício C. Corrigido: alocação dinamica fora do laço de repetição.

--- Semana 5/vetorponteiro.c
+++ Semana 5/vetorponteiro.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <conio.h>
 
 int main(){
     int *vetor;
-    int i, x, y;
-    x=5000;
-    y=0;
-    for (i=0;i<x;i++){
-        vetor=malloc(x*sizeof(int));
-        vetor[i]=y;
-        y++;
-        printf("%d \n", vetor[i]);
+    int k;
+    vetor=(int*)malloc(5000*sizeof(int));
+    for (k=0;k<5000;k++){
+        vetor[k]=k;
+        printf("%d \n", vetor);
     }
 
-    free()
+    free(vetor);
+    return 0;
 }
