ce67b7f77eb39412773c5114eb0d65f63a9e25d9
Pedro Henrique Barauna <barauna38@gmail.com>
2021-12-24 10:56:33 -0300

    deu erro mas ta indo

--- Semana 4/Zero vale zero.c
+++ Semana 4/Zero vale zero.c
@@ -2,7 +2,9 @@
 #include <string.h>
 
 int main(){
-    int x, y, soma, tamanho;
+    int x, y, soma, i;
+    int tamanho;
+    tamanho=5;
     char vetor[soma];
     while (x!=0 && y!=0){
         scanf("%d", &x);
@@ -11,8 +13,15 @@ int main(){
             break;
         }
         soma = x+y;
+        tamanho+=strlen(vetor);
         sprintf(vetor,"%d",soma);
-        
+        for(i=0;i<tamanho;i++){
+            if(vetor[i]!='0'){
+                printf("%c", vetor[i]);
+            }
+            
+        }
+        printf("\n");
     }
     
     return 0;
